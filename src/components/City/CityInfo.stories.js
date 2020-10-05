20bf70a773e2554b75cd485e3da734a687efefaf
Xavier Aguas <aguas.xavier2013a@gmail.com>
2020-10-04 22:59:53 -0500

    Add CityInfo Comp

---
+++ src/components/City/CityInfo.stories.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'typeface-roboto'
+import CityInfo from './CityInfo'
+
+
+export default{
+    title: 'CityInfo',
+    component: CityInfo
+}
+
+export const CityExample = () => {
+
+    return(
+        
+        <CityInfo city={"Quito"} country={"Ecuador"}>
+
+        </CityInfo>
+
+
+    )
+}
