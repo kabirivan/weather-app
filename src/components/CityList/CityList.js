ec70e952aedf0ed05ede66df960b28183e241ff3
Xavier Aguas <aguas.xavier2013a@gmail.com>
2020-10-05 01:20:58 -0500

    Add index files

--- src/components/CityList/CityList.js
+++ src/components/CityList/CityList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import CityInfo from './../CityInfo/CityInfo'
-import Weather from './../Weather/Weather'
+import CityInfo from './../CityInfo'
+import Weather from './../Weather'
 
 
 const renderCityAndCountry = (cityAndCountry) => {
